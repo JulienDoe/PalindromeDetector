80ec3e4bdc1e2eb65d5a7e278b3a4dda112edbab
Julien Doe <jperget@gmail.com>
2022-01-30 07:58:33 +0100

    Ajoutez des fichiers projet.

---
+++ PalindromeDetector/Palindrome.c
@@ -0,0 +1,25 @@
+#define _CRT_SECURE_NO_WARNINGS
+#define _CRT_SECURE_NO_WARNINGS_GLOBALS
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+char taper_mot(char *mot)
+{
+	scanf("%s", mot);
+	return mot;
+}
+
+bool palindrome(char *mot, size_t max)
+{
+	return true;
+}
+
+int main(int argc, char *argv[])
+{
+	char mot[100]="radar";
+	printf("%s\n", mot);
+	
+	return 0;
+}
