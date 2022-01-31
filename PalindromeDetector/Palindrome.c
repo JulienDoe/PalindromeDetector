9e5eec67505df6ea31b9320ce0d3d769302fb4d5
Julien Doe <jperget@gmail.com>
2022-01-31 05:24:33 +0100

    Programme fini

--- PalindromeDetector/Palindrome.c
+++ PalindromeDetector/Palindrome.c
@@ -11,15 +11,29 @@ char taper_mot(char *mot)
 	return mot;
 }
 
-bool palindrome(char *mot, size_t max)
+bool palindrome(char *mot)
 {
+	size_t length = 0;
+	while (mot[length] != '\0')
+	{
+		length++;
+	}
+	for (size_t i = 0; i < length; i++)
+		if (mot[i] != mot[length - 1 - i])
+			return false;
 	return true;
 }
 
 int main(int argc, char *argv[])
 {
-	char mot[100]="radar";
-	printf("%s\n", mot);
-	
+	char mot[100];
+	printf("Ce programme permet de detecter si le mot rentrer est un palindrome.\n"
+		"Veuillez tapez le mot que vous souhaitez soumettre a la machine :");
+	taper_mot(mot);
+	if (palindrome(mot))
+		printf("\nLe mot %s est un palindrome.\n", mot);
+	else
+		printf("\nLe mot %s n'est PAS un palindrome.\n", mot);
+
 	return 0;
 }
